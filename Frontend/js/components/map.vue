dfabc06187001757e1db3ded7200a1e5d57600cd
matthias <matthias@andesia.org>
2018-11-23 02:26:16 +0100

    +Feature: added list of tiles to frontend

--- Frontend/js/components/map.vue
+++ Frontend/js/components/map.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
         <h1>Map</h1>
+
+        <ul id="map-list-1">
+            <li v-bind:key="tile.id" v-for="tile in tiles">
+                {{ tile.x }} | {{ tile.y }}
+            </li>
+        </ul>
+---
+        <div id="map-layer-1">
+            <div v-bind:key="tile.id" v-for="tile in layerZ" class="tile">
+                {{ tile.x }} | {{ tile.y }}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,6 +23,15 @@
             return {
                 // will be a three-dimensional array with map coords
                 tiles: [],
+                targetZ: 0
+            }
+        },
+        computed: {
+            // https://stackoverflow.com/questions/41791482/filter-list-with-vue-js
+            layerZ() {
+                return this.tiles.filter(tile => {
+                    return tile.z == this.targetZ;
+                });
             }
         },
         methods: {
@@ -33,6 +54,12 @@
 </script>
 
 <style>
-
+.tile {
+    display: block;
+    width: 50px;
+    height: 50px;
+    background-color: green;
+    margin: 2px;
+}
 
 </style>
