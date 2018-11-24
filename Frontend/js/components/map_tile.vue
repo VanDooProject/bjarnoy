215331c169f90a6d4a8dd1d5a048f3e5164d8416
onestacked <chrisi.schrefl@gmail.com>
2018-11-24 18:00:37 +0100

    Added map menu prototype

--- Frontend/js/components/map_tile.vue
+++ Frontend/js/components/map_tile.vue
@@ -1,5 +1,5 @@
 <template>
-    <div
+    <div v-on:click="openMenu"
         class="tile"
         v-bind:style="{
                 top: tile.y * -60 + 'px',
@@ -14,6 +14,12 @@
 <script>
     export default {
         props: ['tile'],
+        methods: {
+            openMenu: function(event) {
+                this.$emit('tile_clicked', event);
+                console.log("called")
+            }
+        },
         data: function() {
             return {
                 //tile: {}
@@ -34,7 +40,7 @@
     width: 58px;
     height: 58px;
 
-    background-color: green;
+   background-color: green;
     margin: 2px;
 }
 
---
+++ Frontend/js/components/menu.vue
@@ -0,0 +1,41 @@
+<template>
+    <div 
+        v-bind:style="{
+            top: pos.y - size.y/2 + 'px',
+            left: pos.x - size.x/2 + 'px',
+            width: size.x + 'px',
+            height: size.y + 'px',
+
+            zIndex: 50000
+        }"      
+        class="mapmenu">    
+            pos = ( {{pos.x}} | {{ pos.y }} )
+    </div>
+</template>
+
+<script>
+export default {
+    props:['pos'],
+    data: function() {
+        return {
+            size: {x:150, y:150}
+        }
+    },
+    mounted () {
+        console.log("menu mounted");
+    },
+}
+</script>
+
+<style>
+.mapmenu {
+    display: block;
+
+    position: absolute;
+
+    background-color: oldlace;
+}
+.mapmenu:hover {
+    background-color: lightblue;
+}
+</style>
