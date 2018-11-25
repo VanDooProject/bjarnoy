bca299b8f63cc2add1eb25366044f558048013ba
matthias <matthias@andesia.org>
2018-11-25 04:29:57 +0100

    +Feature: added auth claims and ClaimRoles

--- Backend/ApiServer/Controllers/AuthController.cs
+++ Backend/ApiServer/Controllers/AuthController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using CoreClassLibrary.Models.Auth;
 using CoreClassLibrary.Models.Map;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
@@ -36,6 +37,18 @@ namespace ApiServer.Controllers
             return String.Format("user({0}) is allowed to view page", currentUser);
         }
 
+
+        // GET api/v1/auth/selftest/admin/
+        [HttpGet("selftest/admin/")]
+        [Authorize(Roles = "Admin")]
+        public string GetAdmin()
+        {
+            ClaimsPrincipal currentUser = HttpContext.User;
+
+            return String.Format("user({0}) is allowed to view ADMIN page", currentUser);
+        }
+
+
         // POST api/v1/auth/sign-in
         [AllowAnonymous]
         [HttpPost("sign-in")]
@@ -55,11 +68,23 @@ namespace ApiServer.Controllers
 
         private string BuildToken(UserModel user)
         {
+            // most claims are defined here: http://tools.ietf.org/html/rfc7519#section-4
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Name), // Subject
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Birthdate, user.Birthdate.ToString("yyyy-MM-dd")),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // JWT ID - security measure against replay attacks
+
+                // https://stackoverflow.com/a/38426677/2298744
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(_config["Jwt:Issuer"],
                 _config["Jwt:Issuer"],
+                claims,
                 expires: DateTime.Now.AddMinutes(30),
                 signingCredentials: creds);
 
