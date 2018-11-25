e2c298f711e3a3e7bee6b2879de94024aeb713c7
matthias <matthias@andesia.org>
2018-11-25 03:37:35 +0100

    +Feature: enabled JWT auth

---
+++ Backend/ApiServer/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CoreClassLibrary.Models.Map;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiServer.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class AuthController : Controller
+    {
+
+        // GET api/v1/auth/selftest
+        [HttpGet("selftest/")]
+        [Authorize]
+        public string Get()
+        {
+            ClaimsPrincipal currentUser = HttpContext.User;
+
+            return String.Format("user({0}) is allowed to view page", currentUser);
+        }
+    }
+}
