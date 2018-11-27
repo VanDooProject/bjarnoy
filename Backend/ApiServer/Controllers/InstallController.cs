be12b4597ae3d4fed73743c9270c38eb4e38bf0b
matthias <matthias@andesia.org>
2018-11-27 18:08:55 +0100

    +Feature: added API info page to get current branch version; added volume for mongoDB server

---
+++ Backend/ApiServer/Controllers/InstallController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreClassLibrary.Models.Map;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiServer.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class InstallController : Controller
+    {
+
+        // GET api/v1/install/info
+        [HttpGet("info/")]
+        public List<string> Get()
+        {
+
+            List<string> infoList = new List<string>
+            {
+                Environment.GetEnvironmentVariable("BRANCH")
+            };
+
+            return infoList;
+        }
+    }
+}
