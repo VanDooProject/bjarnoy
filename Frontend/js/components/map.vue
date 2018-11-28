0c040bf520c7a58e940e8426c8bb32254de903fb
onestacked <chrisi.schrefl@gmail.com>
2018-11-28 10:30:26 +0100

    Revert "Movement fix"
    This reverts commit eee0ebdee28c7799aa1463079992fb5e4dda103f.

--- Frontend/js/components/map.vue
+++ Frontend/js/components/map.vue
@@ -8,10 +8,8 @@
         <MapMenu v-bind:pos="menu" v-bind:tile="tile"></MapMenu>
 
         <div id="map">
-            <div id="maptransform">
-                <MapLayer layerZ="2" v-bind:tiles="tiles" v-bind:globalMapOffset="globalMapOffset" @tile_clicked="TileClicked"></MapLayer>
-                <MapLayer layerZ="1" v-bind:tiles="tiles" v-bind:globalMapOffset="globalMapOffset" @tile_clicked="TileClicked"></MapLayer>
-            </div>
+            <MapLayer layerZ="2" v-bind:tiles="tiles" v-bind:globalMapOffset="globalMapOffset" @tile_clicked="TileClicked"></MapLayer>
+            <MapLayer layerZ="1" v-bind:tiles="tiles" v-bind:globalMapOffset="globalMapOffset" @tile_clicked="TileClicked"></MapLayer>
         </div>
     </div>
 </template>
@@ -105,20 +103,17 @@ html, body {
     display: block;
     padding: 0px;
     margin: 0px;
-    min-width: 300%;
-    min-height: 300%;
+    min-width: 100%;
+    min-height: 100%;
     position: fixed;
-    width: 300%;
-    height: 300%;
+    width: 100%;
+    height: 100%;
     left: 0;
     top: 0;
     bottom: 0;
     right: 0;
     z-index: 0;
     transform: rotateX(45deg) rotateZ(45deg);
-}
-#maptransform {
-    transform: translate(0%, 100%)
 }
 
 </style>
