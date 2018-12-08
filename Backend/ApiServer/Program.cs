cba9084fcbba8cd0533a0d47254e6d717caffe00
matthias <matthias@andesia.org>
2018-12-08 19:41:31 +0100

    +Fix: fixed log4net.config hidden by docker volume

--- Backend/ApiServer/Program.cs
+++ Backend/ApiServer/Program.cs
@@ -17,7 +17,7 @@ namespace ApiServer
         public static void Main(string[] args)
         {
             XmlDocument log4netConfig = new XmlDocument();
-            log4netConfig.Load(File.OpenRead("config/log4net.config"));
+            log4netConfig.Load(File.OpenRead("./log4net.config"));
             var repo = log4net.LogManager.CreateRepository(Assembly.GetEntryAssembly(),
                 typeof(log4net.Repository.Hierarchy.Hierarchy));
             log4net.Config.XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
