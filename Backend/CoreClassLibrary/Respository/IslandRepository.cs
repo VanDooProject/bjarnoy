3a95aec9815ad8994e9151ecb170e4a2d90639d0
matthias <matthias@andesia.org>
2018-12-14 03:12:38 +0100

    +Feature: only return tile not whole island when querying tile

--- Backend/CoreClassLibrary/Respository/IslandRepository.cs
+++ Backend/CoreClassLibrary/Respository/IslandRepository.cs
@@ -1,10 +1,14 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using CoreClassLibrary.Factory;
 using CoreClassLibrary.Models.Map;
+using CoreClassLibrary.Models.Map.Biomes;
 using CoreClassLibrary.Models.Map.Tiles;
 using log4net;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 
 namespace CoreClassLibrary.Respository
@@ -39,11 +43,22 @@ namespace CoreClassLibrary.Respository
         public Tile getTile(float x, float y, float z)
         {
             var builder = Builders<Island>.Filter;
-            var filter = builder.Eq("bioms.tiles.Position.X", 10);
-            var result = collection.Find(filter).ToList();
+            var filter = builder.Eq("bioms.tiles.Position.X", x);
+            var projection = Builders<Island>.Projection.Include("bioms.$.tiles");
+            var result = collection.Find(filter).Project(projection).ToList();
 
             if (result.Count == 1)
             {
+                var res = BsonSerializer.Deserialize<Island>(result.First());
+                //var ret = res.bioms.Where(b => b.tiles.Any(t => t.Position.X == 10));
+                Biom biom = res.bioms.First();
+                List<Tile> biomTiles = biom.tiles;
+                //IEnumerable<Tile> tiles = biomTiles.Where(t => (t.Position.X+0.0001) >= x && (t.Position.X - 0.0001) <= x);
+                IEnumerable<Tile> tiles = biomTiles.Where(t => Math.Abs(t.Position.X - x) < 0.0001);
+
+                var ts = tiles.ToList();
+
+                return tiles.FirstOrDefault();
                 //return result[0].Tiles[0];
             }
 
