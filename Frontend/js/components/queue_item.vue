2d8b10a3be4f97bb431da49a4e1464249f68d3da
Onestacked <chrisi.schrefl@gmail.com>
2019-01-07 12:07:07 +0100

    Added reactive Time calculation and Map and Queue updates

--- Frontend/js/components/queue_item.vue
+++ Frontend/js/components/queue_item.vue
@@ -2,7 +2,9 @@
     <div
         class="queueitem"
     >
-        {{entry.tile.building.type}} Level {{entry.tile.building.level}} Until: {{entry.endTime}}
+        {{entry.tile.building.type}} 
+        Level: {{entry.tile.building.level}} 
+        Time left: {{difference}} s
     </div>
 </template>
 
@@ -14,17 +16,29 @@ export default {
     },
     data: function() {
         return {
-            
-        };
+            now: new Date(),
+        }
     },
     computed: {
-        
+        difference() {
+            var end = new Date(this.entry.startTime);
+            var diff = end.getTime() - this.now.getTime();
+            if(diff == 0)
+            {
+                this.$store.dispatch("UpdateQueued");
+                this.$store.dispatch("UpdateMapTiles");
+            }
+            return Math.round((diff) / 1000);
+        }
     },
     methods: {
-    
+        animationCallback: function() {
+            window.requestAnimationFrame(this.animationCallback);
+            this.now = new Date();
+        }
     },
     mounted() {
-        
+        window.requestAnimationFrame(this.animationCallback);
     }
 };
 </script>
