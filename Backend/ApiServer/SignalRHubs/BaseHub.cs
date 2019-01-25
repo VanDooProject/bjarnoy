8b3b1b8cfd299d7649522b175fcef1e4ad6989dd
matthias <matthias@andesia.org>
2019-01-25 16:08:01 +0100

    +Feature: log connections to SignalR to terminal

--- Backend/ApiServer/SignalRHubs/BaseHub.cs
+++ Backend/ApiServer/SignalRHubs/BaseHub.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using log4net;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
@@ -11,10 +12,27 @@ namespace ApiServer.SignalRHubs
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class BaseHub : Hub
     {
+        private ILog logger = LogManager.GetLogger(typeof(BaseHub));
+
+        public override Task OnConnectedAsync()
+        {
+            logger.Info("client connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            logger.Info("client disconnected");
+            return base.OnDisconnectedAsync(exception);
+        }
+
         public async Task SendMessage(
             string message)
         {
+            logger.Info($"got message {message}");
             await Clients.All.SendAsync("newMessage", "anonymous", message);
         }
+
+
     }
 }
