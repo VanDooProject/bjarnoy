777c04c06d07d3fc82e3d2d7d1ed0a3a33488926
Christian <chrisi.schrefl@gmail.com>
2019-02-18 10:38:59 +0100

    Remove an unused CSS class

--- Frontend/js/components/map_tile.vue
+++ Frontend/js/components/map_tile.vue
@@ -140,17 +140,6 @@ export default {
 </script>
 
 <style>
-.tile {
-    position: absolute;
-
-    display: block;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    padding: 0px;
-    margin: 0px;
-}
 .tileimg {
     pointer-events: none;
 }
