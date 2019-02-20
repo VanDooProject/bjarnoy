67ea96345c2b0815596055865899e9cb5e90867b
matthias <matthias@andesia.org>
2019-02-20 11:19:33 +0100

    +Feature: added ResourceController Endpoint to fetch resources of current user

---
+++ Backend/ApiServer/Controllers/ResourceController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreClassLibrary.Controller;
+using CoreClassLibrary.Models.Auth;
+using CoreClassLibrary.Models.Buildings;
+using CoreClassLibrary.Models.Map;
+using CoreClassLibrary.Models.Resources;
+using CoreClassLibrary.Respository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiServer.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ResourceController : GameAPIController
+    {
+
+        // GET api/v1/resource/user
+        [HttpGet("user/")]
+        [Authorize]
+        public UserResources GetResourcesOfCurrentUser()
+        {
+            UserModel user = getCurretUser();
+
+            return user.UserResources;
+        }
+    }
+}
