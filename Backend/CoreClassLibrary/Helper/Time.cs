f97381a4f66d77a6a1098019b36f8af38301bbc9
matthias <matthias@andesia.org>
2019-02-26 21:55:58 +0100

    +Feature: added missing time class and unittests for PlayingEntityRes

---
+++ Backend/CoreClassLibrary/Helper/Time.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreClassLibrary.Helper
+{
+    /// <summary>
+    /// Used for getting DateTime.Now(), time is changeable for unit testing
+    /// Source: https://stackoverflow.com/a/9911500/2298744
+    /// </summary>
+    public static class Time
+    {
+        /// <summary> Normally this is a pass-through to DateTime.Now, but it can be overridden with SetDateTime( .. ) for testing or debugging.
+        /// </summary>
+        public static Func<DateTime> NowFunc = () => DateTime.Now;
+
+        public static DateTime Now => NowFunc();
+
+        /// <summary> Set time to return when SystemTime.Now() is called.
+        /// </summary>
+        public static void SetDateTime(DateTime dateTimeNow)
+        {
+            NowFunc = () => dateTimeNow;
+        }
+
+        /// <summary> Resets SystemTime.Now() to return DateTime.Now.
+        /// </summary>
+        public static void ResetDateTime()
+        {
+            NowFunc = () => DateTime.Now;
+        }
+    }
+}
---
+++ Backend/UnitTests/PlayingEntityResources.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using CoreClassLibrary.Helper;
+using CoreClassLibrary.Models.Resources;
+using NUnit.Framework;
+using Pose;
+
+namespace UnitTests
+{
+    class PlayingEntityResources
+    {
+        [Test]
+        public void ResourceGeneration()
+        {
+            // Arrange
+            int year = 1970;
+            int hour = 0;
+            int minute = 0;
+            int sec = 0;
+
+            var fixedTime = new DateTime(year, 1, 1, hour, minute, sec);
+            Time.SetDateTime(fixedTime); // this could fail (other tests) if tests run in parallel
+            Assert.AreEqual(year, Time.Now.Year);
+
+            // setup test
+            int production = 10;
+            int alreadyStoredRes = 100;
+
+            var res = new UserResources()
+            {
+                LastResourceStorageRefresh = Time.Now.AddHours(-1),
+                ResourceStorageCapacity = new Resources()
+                {
+                    wood = 1000
+                },
+                HourlyResourceProduction = new Resources()
+                {
+                    wood = production
+                },
+                ResourceStoredAtLastCalculation = new Resources()
+                {
+                    wood = alreadyStoredRes
+                }
+            };
+
+            // check
+            double TOLERANCE = 0.001;
+            Debug.Assert(Math.Abs(res.ResourcesStoredCurrently.wood - (alreadyStoredRes + production)) < TOLERANCE);
+        }
+    }
+}
